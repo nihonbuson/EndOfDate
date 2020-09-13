e24df4b452b3ca01b75bac2969d28d3686ce2a57
nihonbuson <nihonbuson@gmail.com>
2020-09-13 16:24:39 +0900

    初期コミット

---
+++ src/main/java/DeliveryDate.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
+public class DeliveryDate {
+    public LocalDate getDeliveryDate(){
+        LocalDate localDate = LocalDate.now();
+        int day = localDate.getDayOfMonth();
+        Month month = localDate.getMonth();
+        int year = localDate.getYear();
+
+        if(day < 10){
+            return LocalDate.of(localDate.getYear(), localDate.getMonth(), 15);
+        }
+
+        if(day >= 25){
+            month.plus(1L);
+        } else if (month.equals(Month.DECEMBER) && day >= 20) {
+            month.plus(1L);
+        }
+
+        int lastDay;
+        if(month.equals(Month.APRIL)) {
+            lastDay = 31;
+        } else if(month.equals(Month.JUNE)){
+            lastDay = 31;
+        } else if(month.equals(Month.SEPTEMBER)){
+            lastDay = 31;
+        } else if(month.equals(Month.NOVEMBER)){
+            lastDay = 31;
+        } else if(month.equals(Month.FEBRUARY)){
+            if(year%4 == 0){
+                lastDay = 29;
+            } else {
+                lastDay = 28;
+            }
+        } else {
+            lastDay = 30;
+        }
+        return LocalDate.of(localDate.getYear(), localDate.getMonth(), lastDay);
+    }
+}
