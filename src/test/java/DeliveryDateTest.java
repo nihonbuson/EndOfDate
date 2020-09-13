427422f07501da430132f20611d1f49a0f9e7471
nihonbuson <nihonbuson@gmail.com>
2020-09-14 01:06:49 +0900

    小の月のテストを拡充する

--- src/test/java/DeliveryDateTest.java
+++ src/test/java/DeliveryDateTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -6,12 +7,59 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DeliveryDateTest {
 
-    @Test
-    void _小の月の月末になる場合のテスト() {
-        FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
-        deliveryDate.setDate(LocalDate.of(2020,9,13));
-        LocalDate actualDate = deliveryDate.getDeliveryDate();
-        assertEquals(LocalDate.of(2020,9,30),actualDate);
+
+    @Nested
+    class _小の月の月末 {
+        @Test
+        void _4月の月末になる場合のテスト() {
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2020,4,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,4,30),actualDate);
+        }
+
+        @Test
+        void _6月の月末になる場合のテスト() {
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2020,6,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,6,30),actualDate);
+        }
+
+        @Test
+        void _9月の月末になる場合のテスト() {
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2020,9,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,9,30),actualDate);
+        }
+
+        @Test
+        void _11月の月末になる場合のテスト() {
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2020,11,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,11,30),actualDate);
+        }
+    }
+
+    @Nested
+    class _2月の月末 {
+        @Test
+        void _4で割り切れない年の2月の月末になる場合のテスト() {
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2019,2,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2019,2,28),actualDate);
+        }
+
+        @Test
+        void _4で割り切れる年の2月の月末になる場合のテスト() {
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2020,2,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,2,29),actualDate);
+        }
     }
 
     @Test
