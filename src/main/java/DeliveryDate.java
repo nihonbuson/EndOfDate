327eba64744050f58b7863a98bf3e05cf70fd504
nihonbuson <nihonbuson@gmail.com>
2020-09-13 16:57:43 +0900

    月末の部分のテストで切り出す

--- src/main/java/DeliveryDate.java
+++ src/main/java/DeliveryDate.java
@@ -18,6 +18,11 @@ public class DeliveryDate {
             month.plus(1L);
         }
 
+        int lastDay = getLastDay(year, month);
+        return LocalDate.of(localDate.getYear(), localDate.getMonth(), lastDay);
+    }
+
+    int getLastDay(int year, Month month) {
         int lastDay;
         if(month.equals(Month.APRIL)) {
             lastDay = 30;
@@ -36,6 +41,6 @@ public class DeliveryDate {
         } else {
             lastDay = 31;
         }
-        return LocalDate.of(localDate.getYear(), localDate.getMonth(), lastDay);
+        return lastDay;
     }
 }
--- src/test/java/DeliveryDateTest.java
+++ src/test/java/DeliveryDateTest.java
@@ -1,21 +1,34 @@
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeliveryDateTest {
 
+//    @Test
+//    void _小の月の月末になる場合のテスト() {
+//        LocalDate actualDate = new DeliveryDate().getDeliveryDate();
+//        assertEquals(LocalDate.of(2020,9,30),actualDate);
+//    }
+//
+//    @Test
+//    void _大の月の月末になる場合のテスト() {
+//        LocalDate actualDate = new DeliveryDate().getDeliveryDate();
+//        assertEquals(LocalDate.of(2020,10,31),actualDate);
+//    }
+
+
     @Test
-    void _小の月の月末になる場合のテスト() {
-        LocalDate actualDate = new DeliveryDate().getDeliveryDate();
-        assertEquals(LocalDate.of(2020,9,30),actualDate);
+    void _小の月の月末の日付のテスト() {
+        int lastDay = new DeliveryDate().getLastDay(2020, Month.SEPTEMBER);
+        assertEquals(30,lastDay);
     }
 
     @Test
-    void _大の月の月末になる場合のテスト() {
-        LocalDate actualDate = new DeliveryDate().getDeliveryDate();
-        assertEquals(LocalDate.of(2020,10,31),actualDate);
+    void _大の月の月末の日付のテスト() {
+        int lastDay = new DeliveryDate().getLastDay(2020, Month.OCTOBER);
+        assertEquals(31,lastDay);
     }
-
 }
