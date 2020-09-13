7400f86c25e2c01c4b3888ce99091d33f1dabc9a
nihonbuson <nihonbuson@gmail.com>
2020-09-13 16:43:13 +0900

    とりあえずテストを作る

---
+++ src/test/java/DeliveryDateTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeliveryDateTest {
+
+    @Test
+    void _配送日のテスト() {
+        new DeliveryDate().getDeliveryDate();
+        assertEquals(1,1);
+    }
+}
